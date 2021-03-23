39a32603cda361e082f71a347e3b95021d32518b
Arjay Bayona <arjayby1@gmail.com>
2021-03-24 01:29:31 +0800

    🎨 Customize theme

---
+++ theme/index.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#e2e0ff",
+      200: "#b6b1ff",
+      300: "#8982ff",
+      400: "#6c63ff",
+      500: "#6159e6",
+      600: "#4c45b3",
+      700: "#363280",
+      800: "#201e4c",
+      900: "#0b0a19",
+    },
+  },
+});
