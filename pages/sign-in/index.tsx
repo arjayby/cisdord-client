b4ad556b57bd088f0a7fbbcfb4c4a4a4eac061d5
Arjay Bayona <arjayby1@gmail.com>
2021-04-01 11:14:38 +0800

    🎨 Add sign in page

---
+++ pages/sign-in/index.tsx
@@ -0,0 +1,117 @@
+import React, { FormEvent, useRef, useState } from "react";
+import Link from "next/link";
+import Layout from "@components/Layout";
+import {
+  Alert,
+  AlertIcon,
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import api from "@api";
+
+const SignIn: React.FC = () => {
+  const usernameRef = useRef<HTMLInputElement>();
+  const passwordRef = useRef<HTMLInputElement>();
+
+  const [errors, setErrors] = useState<{
+    username?: string;
+    password?: string;
+  }>({});
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [loginError, setLoginError] = useState("");
+
+  const handleShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const handleSignIn = async (e: FormEvent) => {
+    e.preventDefault();
+
+    setErrors({});
+
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+
+    if (!username || !password) {
+      setErrors({
+        username: "Not valid 😐",
+        password: "Not valid 😐",
+      });
+      return;
+    }
+
+    try {
+      await api.app.authenticate({
+        strategy: "local",
+        username,
+        password,
+      });
+    } catch (e) {
+      setLoginError(e.message);
+    }
+  };
+
+  return (
+    <Layout title="Sign in | cisdord">
+      <Box h="100%" display="grid" placeItems="center">
+        <Box maxW="450" w="100%">
+          <SimpleGrid as="form" gap="3" onSubmit={handleSignIn}>
+            {loginError && (
+              <Alert mb="5" status="error">
+                <AlertIcon />
+                {loginError}
+              </Alert>
+            )}
+            <FormControl id="username">
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input ref={usernameRef} type="username" />
+              <FormHelperText>{errors.username}</FormHelperText>
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <InputGroup>
+                <Input
+                  ref={passwordRef}
+                  type={showPassword ? "text" : "password"}
+                />
+                <InputRightElement>
+                  <Button variant="ghost" onClick={handleShowPassword}>
+                    {showPassword ? "🙉" : "🙈"}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <FormHelperText>{errors.password}</FormHelperText>
+            </FormControl>
+            <Button mt="5" type="submit" colorScheme="brand">
+              Sign in
+            </Button>
+            <Text textAlign="center">or</Text>
+            <Button>Sign in with Github</Button>
+            <Link href="/sign-up">
+              <Button
+                as="a"
+                href="/sign-up"
+                mt="5"
+                variant="ghost"
+                fontWeight="normal"
+              >
+                Don't have an account? Sign up here.
+              </Button>
+            </Link>
+          </SimpleGrid>
+        </Box>
+      </Box>
+    </Layout>
+  );
+};
+
+export default SignIn;
