59a0b7ca4c480f570ac2c28111a6a07e571007af
Arjay Bayona <arjayby1@gmail.com>
2021-04-10 13:17:25 +0800

    ⚡ Consume chats context provider

--- pages/channels/me/index.tsx
+++ pages/channels/me/index.tsx
@@ -20,99 +20,41 @@ import ChannelDescription from "@components/ChannelDescription";
 import withAuth from "@components/withAuth";
 import { MdSearch } from "react-icons/md";
 import { useAuthContext } from "contexts/AuthContext";
-import { ChannelType } from "types/channel";
-import { MessageType } from "types/message";
-import { formatRelative } from "date-fns";
-import { locale } from "utils";
-import api from "@api";
+import { useChatsContext } from "contexts/ChatsContext";
+import { ChatType } from "types/chat";
+import { getDayName } from "utils/date";
+import { createMessage } from "api/messages";
 
 const Me: React.FC = () => {
   const router = useRouter();
   const { user } = useAuthContext();
+  const { chats } = useChatsContext();
 
-  const [channels, setChannels] = useState<ChannelType[]>([]);
-  const [selectedChannel, setSelectedChannel] = useState<ChannelType>();
-  const [selectedChannelMessages, setSelectedChannelMessages] = useState<
-    MessageType[]
-  >([]);
-
-  const getUserChannels = async () => {
-    const members = await api.members.find({
-      query: {
-        userId: user.id,
-      },
-    });
-
-    const channels = await api.channels.find({
-      query: {
-        id: {
-          $in: members.data.map((member) => member.channelId),
-        },
-      },
-    });
-
-    setChannels(channels.data);
-  };
-
-  const getChannelMessages = async () => {
-    const data = await api.messages.find({
-      query: {
-        $sort: {
-          createdAt: -1,
-        },
-        channelId: selectedChannel.id,
-        include: true,
-      },
-    });
-    setSelectedChannelMessages(data.data);
-  };
+  const [selectedChat, setSelectedChat] = useState<ChatType>();
 
   const setSelectedChannelFromRouterQuery = () => {
     const c = router.query.c as string;
-    const channel = channels.find((channel) => channel.shortId === c);
-    setSelectedChannel(channel);
+    const chat = chats.find((chat) => chat.channel.shortId === c);
+    setSelectedChat(chat);
   };
-
-  useEffect(() => {
-    getUserChannels();
-  }, []);
-
-  useEffect(() => {
-    if (selectedChannel) {
-      getChannelMessages();
-    }
-  }, [selectedChannel]);
 
   useEffect(() => {
     if (router.query.c) {
       setSelectedChannelFromRouterQuery();
     } else {
-      setSelectedChannel(undefined);
+      setSelectedChat(undefined);
     }
-  }, [channels, router.query.c]);
-
-  useEffect(() => {
-    api.messages.on("created", (message) => {
-      setSelectedChannelMessages((prev) => [message, ...prev]);
-    });
-
-    return () => {
-      api.messages.removeListener("created");
-    };
-  }, []);
+  }, [router.query.c, chats]);
 
   const handleRoutePushToChannels = (shortId: string) => {
-    if (selectedChannel?.shortId !== shortId) {
+    if (selectedChat?.channel?.shortId !== shortId) {
       router.push(`/channels/me?c=${shortId}`);
     }
   };
 
   const handleSendMessage = async (message: string) => {
     try {
-      await api.messages.create({
-        channelId: selectedChannel.id,
-        body: message,
-      });
+      createMessage(selectedChat.channel.id, message);
     } catch (e) {
       console.error("Send message failed 😠", e);
     }
@@ -135,14 +77,16 @@ const Me: React.FC = () => {
             </InputGroup>
             <Box my="7" />
             <Grid gap={3}>
-              {channels.map((channel) => (
+              {chats.map(({ channel, messages }) => (
                 <Box
                   key={`channel-${channel.id}`}
                   as="button"
                   onClick={() => handleRoutePushToChannels(channel.shortId)}
                   p="5"
                   bg={
-                    channel.id === selectedChannel.id ? "brand.50" : "#f9f9f9"
+                    channel.id === selectedChat?.channel?.id
+                      ? "brand.50"
+                      : "#f9f9f9"
                   }
                   rounded="lg"
                 >
@@ -160,18 +104,15 @@ const Me: React.FC = () => {
                         />
                         <Box mx="2" />
                         <Text fontSize="xs" color="gray.500" isTruncated>
-                          {formatRelative(
-                            new Date(channel.updatedAt),
-                            new Date(),
-                            { locale }
-                          )}
+                          {getDayName(channel.updatedAt)}
                         </Text>
                       </Flex>
                       <Box my="0.5" />
                       <ChannelDescription
-                        description={channel.description}
-                        isTruncated
+                        description={messages[0]?.body}
+                        textAlign="start"
                         fontSize="sm"
+                        isTruncated
                         maxW="180px"
                       />
                     </Flex>
@@ -183,17 +124,17 @@ const Me: React.FC = () => {
           </Box>
           <Box mx="2" />
           <Box flex={1}>
-            {selectedChannel ? (
+            {selectedChat ? (
               <Chat
                 user={user}
-                channel={selectedChannel}
-                messages={selectedChannelMessages}
+                channel={selectedChat.channel}
+                messages={selectedChat.messages}
                 onSendMessage={handleSendMessage}
               />
             ) : (
               <Grid h="100%" placeItems="center">
                 <Text>
-                  {channels.length > 0
+                  {chats.length > 0
                     ? "Select a channel to open conversation"
                     : "Join the other channel or create your own"}
                 </Text>
